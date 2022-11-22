162cdb3ae067f23f18ef9cb3cf640fec6f6a745a
srraDs <darren_longno@dlsu.edu.ph>
2022-11-23 01:34:39 +0800

    folder placements

---
+++ routes/home.js
@@ -0,0 +1,26 @@
+function toCCSProfs() {
+    window.location.href="ccs_profs.html";
+    }
+  
+  function toCLAProfs() {
+      window.location.href="cla_profs.html";
+  }
+  
+  function toCOEProfs() {
+    window.location.href="coe_profs.html";
+  }
+  
+  function toCOBProfs() {
+    window.location.href="cob_profs.html";
+  }
+  function toCOSProfs() {
+    window.location.href="cos_profs.html";
+  }  
+  
+  function toCEDProfs() {
+    window.location.href="ced_profs.html";
+  }
+  function toSOEProfs() {
+    window.location.href="soe_profs.html";
+  }  
+  
