0781e08e485ffb0bdbc0b8bacc0aa03dba68d18a
ssamu801 <114760962+ssamu801@users.noreply.github.com>
2022-11-23 00:21:39 +0800

    Update profListController.js

--- controllers/profListController.js
+++ controllers/profListController.js
@@ -1,29 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
-const app = express();
-
-mongoose.connect("mongodb://localhost:27017/profsToPick")
-
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
-
 const Prof = require("../models/profSchema");
 
-const profList = (req, res) => {
-
-    Prof.find({}, function(err, rows){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render('prof_list',{prof_profile: rows})
-        }
-        });
-}
-
 module.exports = {
-    profList
+    profList: function(req, res){
+        Prof.find({},function(err, result){
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render('prof_list',{prof_profile: result})
+            }
+        })
+    }
 }
