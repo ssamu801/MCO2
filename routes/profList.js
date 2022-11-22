7e2c323913b2dddf8065f7ee88fc5b24ff997256
ssamu801 <114760962+ssamu801@users.noreply.github.com>
2022-11-23 00:26:00 +0800

    Update profList.js

--- routes/profList.js
+++ routes/profList.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {profList} = require('../controller/profListController');
+const {profList} = require('../controllers/profListController');
 const router = express.Router();
 router.get('/profList', profList);
 module.exports = router;
